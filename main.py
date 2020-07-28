6538622cbddc53401ede4e4fc2a09a86e00ea63b
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-07-29 00:02:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,29 @@ word_dict = {
     'place':['park','desert','store','restuarant','waterfall']
 }
 
+def get_word(type, local_dict):
+    words = local_dict[type]
+    cnt = len(words)-1
+    index = randint(0, cnt)
+    return local_dict[type].pop(index)
 
+def create_story():
+    local_dict = copy.deepcopy(word_dict)
+    return story.format(
+        get_word('adjective',local_dict),
+        get_word('sports noun',local_dict),
+        get_word('city name',local_dict),
+        get_word('noun',local_dict),
+        get_word('noun',local_dict),
+        get_word('action verb',local_dict),
+        get_word('noun',local_dict),
+        get_word('place',local_dict),
+        get_word('noun',local_dict),
+        get_word('adjective',local_dict),
+        get_word('noun',local_dict),
+        get_word('adjective',local_dict),
+        get_word('noun',local_dict)
+    )
 
 print('STORY 1:')
 print(create_story())
